f5a4fc7d720d795656a4ef264e1d213f1a7a4b5a
HoltGavyn <gavynholt@gmail.com>
2021-03-21 17:44:00 -0400

    randomized gameWordsList on gamePage, created userListInTurnOrder array

--- teamUserClass.js
+++ teamUserClass.js
@@ -75,10 +75,26 @@ function getTeamsListFromJSON(teamsData) {
         output[teamIdx] = new Team(teamsData[teamIdx].teamName, teamsData[teamIdx].members, teamsData[teamIdx].score);
         // create new User Objects for each user of a given team
         for (let memberIdx = 0; memberIdx < teamsData[teamIdx].members.length; memberIdx++) {
-            teamsData[teamIdx].members[memberIdx] = new User(teamsData[teamIdx].members[memberIdx].userName, teamsData[teamIdx].members[memberIdx].wordList)
+            teamsData[teamIdx].members[memberIdx] = new User(teamsData[teamIdx].members[memberIdx].userName, teamsData[teamIdx].members[memberIdx].wordList);
         }
     }
     return output;
+}
+
+function shuffle(array) {
+    //https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
 }
 
 function getWordsListAllTeams(teams) {
@@ -88,5 +104,19 @@ function getWordsListAllTeams(teams) {
                 output.push(...currentMember.getWordList());
         }
     }
+    shuffle(output);
     return output;
 }
+
+function getUserListInTurnOrder(teams) {
+    let output = [];
+    let teamA = teams[0].getMembersList();
+    let teamB = teams[1].getMembersList();
+    for (let i=0; i<teamA.length; i++) {
+        output.push(teamA[i]);
+        output.push(teamB[i]);
+    }
+    //doesn't account for inbalanced teams - may breaks
+    return output;
+}
+
