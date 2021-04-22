1ea1435287ab4c26bc253ab9b8966bf1b0f4a37c
nickholt15 <nickholt15@gmail.com>
2021-04-21 21:10:15 -0400

    first react commit with very basic functionality

---
+++ src/components/Word.js
@@ -0,0 +1,14 @@
+import { FaTimes } from 'react-icons/fa'
+
+const Word = ( {word, onDelete} ) => {
+    return (
+        <div className='word'>
+            <h3>
+                {word.text} <FaTimes onClick={() => onDelete(word.id)} /> 
+            </h3>
+            <p>{word.user}</p>
+        </div>
+    )
+}
+
+export default Word
