aa0022fe729caf1ce09c60f10ef987190f0bc697
Nick Holt <nickholt15@gmail.com>
2021-04-26 22:41:39 -0400

    finished tutorial components and added mock backend

--- src/App.js
+++ src/App.js
@@ -1,45 +1,69 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Words from './components/Words'
 import AddWord from './components/AddWord'
+import Footer from './components/Footer'
+import About from './components/About'
 
 const App = () => {
-    const [words, setWords] = useState([
-    {
-        id: 1,
-        text: 'Stacey',
-        user: 'sergeantskills'
-    },
-    {
-        id: 2,
-        text: 'Nick',
-        user: 'sergeantskills'
-    },
-    {
-        id: 3,
-        text: 'Bradford',
-        user: 'chaloobi'
+    const [showAddWord, setShowAddWord] = useState(false)
+    const [words, setWords] = useState([])
+
+    useEffect(() => {
+        const getWords = async () => {
+            const wordsFromServer = await fetchWords()
+            setWords(wordsFromServer)
+        }
+        getWords()
+    }, [])
+
+    // Fetch Words
+    const fetchWords = async () => {
+        const res = await fetch('http://localhost:5000/words')
+        const data = await res.json()
+        return data
     }
-    ])
 
     // Add Word
-    const addWord = (word) => {
-        const id = Math.floor(Math.random()*10000) + 1
-        const newWord = { id, ...word }
-        setWords([...words, newWord])
+    const addWord = async (word) => {
+        const res = await fetch('http://localhost:5000/words', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(word)
+        })
+        
+        const data = await res.json()
+
+        setWords([...words, data])
     }
 
     // Delete Word
-    const deleteWord = (id) => {
+    const deleteWord = async (id) => {
+        await fetch(`http://localhost:5000/words/${id}`, {
+            method: 'DELETE'
+        })
+
         setWords(words.filter((word) => word.id !== id))
     }
 
     return (
-        <div className="container">
-            <Header />
-            <AddWord onAdd={addWord} />
-            {words.length > 0 ? <Words words={words} onDelete={deleteWord} /> : 'No words to show... yet!! :)'}
-        </div>
+        <Router>
+            <div className="container">
+                <Header onAdd={() => setShowAddWord(!showAddWord)} showAdd={showAddWord} />
+                
+                <Route path='/' exact render={(props) =>(
+                    <>
+                        {showAddWord && <AddWord onAdd={addWord} />}
+                        {words.length > 0 ? <Words words={words} onDelete={deleteWord} /> : 'No words to show... yet!! :)'}
+                    </>
+                )} />
+                <Route path='/about' component={About}/>
+                <Footer />
+            </div>
+        </Router>
     );  
 }
 
