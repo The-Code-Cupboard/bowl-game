42f6f104badcf3eb808112598cdf2ee9e90e1382
Gavyn Holt <gavynholt@gmail.com>
2021-05-24 10:31:02 -0400

    fixed cookies functions for setUserId() - much more succinct now

--- src/services/helper_functions.js
+++ src/services/helper_functions.js
@@ -1,5 +1,12 @@
 import { nanoid } from "nanoid";
+import * as cookieServices from "./cookie_services";
 
 export const setUserId = () => {
-  return nanoid();
+  //First, check if cookie of "userId" exists in browser
+  if (cookieServices.getCookie("userId") === null) {
+    // if not, create a cookie
+    cookieServices.setCookie("userId", nanoid(), 1);
+  }
+  //In all cases, userId is now set as a cookie to get
+  return cookieServices.getCookie("userId");
 };
