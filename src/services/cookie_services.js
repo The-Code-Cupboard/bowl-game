9b7eb6652d3aa401c98408dda7138b90b312871e
Gavyn Holt <gavynholt@gmail.com>
2021-07-17 20:48:45 -0400

    changed quotes to template literals

--- src/services/cookie_services.js
+++ src/services/cookie_services.js
@@ -5,7 +5,7 @@ export const setCookie = (name, value, days) => {
   if (days) {
     let date = new Date();
     date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-    expires = "; expires=" + date.toUTCString();
+    expires = `; expires=${date.toUTCString()}`;
   }
   document.cookie = name + "=" + (value || "") + expires + "; path=/";
 };
@@ -22,5 +22,5 @@ export const getCookie = (name) => {
 };
 
 export const eraseCookie = (name) => {
-  document.cookie = name + "=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+  document.cookie = `${name}=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
 };
